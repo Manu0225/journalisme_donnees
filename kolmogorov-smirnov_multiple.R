aea69cb0149a70afe4871303ed4c55d4eaa7686f
Manu0225 <34193773+Manu0225@users.noreply.github.com>
2021-02-11 14:41:20 +0100

    Update kolmogorov-smirnov_multiple.R
    utf8

--- kolmogorov-smirnov_multiple.R
+++ kolmogorov-smirnov_multiple.R
@@ -1,5 +1,5 @@
-source("init_données.R",encoding = "utf8")
-source("évolutions.R",encoding = "utf8")
+source("init_donnÃ©es.R",encoding = "utf8")
+source("Ã©volutions.R",encoding = "utf8")
 
 ## test kolmogorov - smirnov multiple ?
 
@@ -8,25 +8,25 @@ library("kSamples")
 #unique(df_paca$`Siren EPCI`)
 
 
-df_évol_paca = df_paca[c(1, 2, 3, 4, 10, 16:27)]
+df_Ã©vol_paca = df_paca[c(1, 2, 3, 4, 10, 16:27)]
 
-df_évol_paca = reformat_data_frame(normaliser(df_évol_paca))
+df_Ã©vol_paca = reformat_data_frame(normaliser(df_Ã©vol_paca))
 
-df_évol_paca["Siren EPCI"] = df_paca$`Siren EPCI`
+df_Ã©vol_paca["Siren EPCI"] = df_paca$`Siren EPCI`
 
-#df_paca[df_paca$`Code département` %in% unique(df_paca$`Code département`),]
+#df_paca[df_paca$`Code dÃ©partement` %in% unique(df_paca$`Code dÃ©partement`),]
 #"04" "05" "06" "13" "83" "84"
-select_département = function(df, département){
-  return (df[df$`Code département` == département,]$Delta)
+select_dÃ©partement = function(df, dÃ©partement){
+  return (df[df$`Code dÃ©partement` == dÃ©partement,]$Delta)
 }
 
 ad.test(
-  select_département(df_évol_paca, "04"),
-  select_département(df_évol_paca, "05"),
-  select_département(df_évol_paca, "06"),
-  select_département(df_évol_paca, "13"),
-  select_département(df_évol_paca, "83"),
-  select_département(df_évol_paca, "84"),
+  select_dÃ©partement(df_Ã©vol_paca, "04"),
+  select_dÃ©partement(df_Ã©vol_paca, "05"),
+  select_dÃ©partement(df_Ã©vol_paca, "06"),
+  select_dÃ©partement(df_Ã©vol_paca, "13"),
+  select_dÃ©partement(df_Ã©vol_paca, "83"),
+  select_dÃ©partement(df_Ã©vol_paca, "84"),
   method = "exact", dist = FALSE
   #, Nsim = 1000
 )
@@ -37,24 +37,24 @@ select_EPCI = function(df, EPCI_num){
 # "200054807" "241300417" "241300375" "200035087"
 
 ad.test(
-  select_EPCI(df_évol_paca, EPCI_num = "200054807"),
-  select_EPCI(df_évol_paca, EPCI_num = "200035087"),
-  select_EPCI(df_évol_paca, EPCI_num = "241300375"),
-  select_EPCI(df_évol_paca, EPCI_num = "241300417"),
+  select_EPCI(df_Ã©vol_paca, EPCI_num = "200054807"),
+  select_EPCI(df_Ã©vol_paca, EPCI_num = "200035087"),
+  select_EPCI(df_Ã©vol_paca, EPCI_num = "241300375"),
+  select_EPCI(df_Ã©vol_paca, EPCI_num = "241300417"),
   method = "exact", dist = FALSE
 )
 
 ### variances sur chaque DOMTOM ?
 
-df["Proportion de locaux éligibles à la fibre"] =
-  df$`T3 2020` / df$`Meilleure estimation des locaux à date`
-#df_évol_paca$`2020/07/01`
-#df_évol_guadeloupe$`2020/07/01`
+df["Proportion de locaux Ã©ligibles Ã  la fibre"] =
+  df$`T3 2020` / df$`Meilleure estimation des locaux Ã  date`
+#df_Ã©vol_paca$`2020/07/01`
+#df_Ã©vol_guadeloupe$`2020/07/01`
 # 971 972 973 974 976 
 
-for (dép_num in c(971,972,973,974,976,977,978)){
-  print(paste0("dép numéro :",dép_num))
-  print(var(df[df$`Code département`==paste0(dép_num),]$`Proportion de locaux éligibles à la fibre`))
+for (dÃ©p_num in c(971,972,973,974,976,977,978)){
+  print(paste0("dÃ©p numÃ©ro :",dÃ©p_num))
+  print(var(df[df$`Code dÃ©partement`==paste0(dÃ©p_num),]$`Proportion de locaux Ã©ligibles Ã  la fibre`))
 }
 
 #ks.test()
